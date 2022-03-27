7174e0a6bb7f00cd28bc72b5e6d128085e31a343
QRWells <mr.qrwang@gmail.com>
2022-03-28 05:46:50 +0900

    1. Optimized data structure used. 2. Implemented basic functions for Process

--- Simulator/Process.cs
+++ Simulator/Process.cs
@@ -19,6 +19,12 @@ public enum TaskType
     CpuBounding
 }
 
+public class Task
+{
+    public int Duration;
+    public TaskType Type;
+}
+
 public enum ProcessState
 {
     Runnable,
@@ -29,74 +35,135 @@ public enum ProcessState
 
 public class Process
 {
-    /// <summary>
-    ///     Time at which the process arrives in the ready queue.
-    /// </summary>
-    public int ArriveTime { get; private set; }
+    private readonly Queue<Task> _tasks = new();
 
-    /// <summary>
-    ///     Time required by a process for CPU execution.
-    /// </summary>
-    public int BurstTime { get; init; }
-
-    public int TimeHaveBurst { get; private set; }
-
-    public int RemainingTime => BurstTime - TimeHaveBurst;
+    public Process(int processId, int arriveTime)
+    {
+        ProcessId = processId;
+        ArriveTime = arriveTime;
+    }
 
     /// <summary>
     ///     Time at which process completes its execution.
     /// </summary>
-    private int _completeTime;
+    public int CompleteTime { get; private set; } = -1;
 
-    private Job _job;
+    /// <summary>
+    ///     Time at which the process arrives in the ready queue.
+    /// </summary>
+    public int ArriveTime { get; }
 
-    public Process(int processId, Job job, int arriveTime)
-    {
-        ProcessId = processId;
-        _job = job;
-        ArriveTime = arriveTime;
-        BurstTime = job.TotalDuration;
-    }
+    /// <summary>
+    ///     Time required by a process for CPU execution.
+    /// </summary>
+    public int BurstTime { get; private set; }
 
+    /// <summary>
+    ///     Time has been executed.
+    /// </summary>
+    public int TimeHaveBurst { get; private set; }
+
+    /// <summary>
+    ///     Remaining time for execution.
+    /// </summary>
+    public int RemainingTime => BurstTime - TimeHaveBurst;
+
+    /// <summary>
+    ///     Time from process arrival to its first execution.
+    /// </summary>
+    public int ResponseTime { get; } = -1;
+
+    /// <summary>
+    ///     total duration of the CPU bound tasks.
+    /// </summary>
+    public int CpuDuration { get; private set; }
+
+    /// <summary>
+    ///     total duration of the I/O bound tasks.
+    /// </summary>
+    public int IoDuration { get; private set; }
+
+    /// <summary>
+    ///     Optional process name.
+    /// </summary>
     public string? Name { get; set; }
+
+    /// <summary>
+    ///     the priority of the process.
+    /// </summary>
     public int Priority { get; init; } = 0;
+
+    /// <summary>
+    ///     the current state of the process.
+    /// </summary>
     public ProcessState State { get; private set; } = ProcessState.Runnable;
 
+    /// <summary>
+    ///     the id of the process.
+    /// </summary>
     public int ProcessId { get; set; }
 
     /// <summary>
     ///     Time Difference between completion time and arrival time.
     /// </summary>
-    public int TurnaroundTime => _completeTime - ArriveTime;
+    public int TurnaroundTime => CompleteTime - ArriveTime;
 
     /// <summary>
     ///     Time Difference between turn around time and burst time.
     /// </summary>
     public int WaitTime => TurnaroundTime - BurstTime;
 
+    /// <summary>
+    ///     Indicate if the process is finished.
+    /// </summary>
     public bool IsCompleted => State == ProcessState.Terminated;
+
+    public void AddTask(int duration, TaskType taskType = TaskType.CpuBounding)
+    {
+        _tasks.Enqueue(new Task { Type = taskType, Duration = duration });
+        CpuDuration += taskType == TaskType.CpuBounding ? duration : 0;
+        IoDuration += taskType == TaskType.IoBounding ? duration : 0;
+        BurstTime += duration;
+    }
 
     public void SetComplete(int currentTime)
     {
         State = ProcessState.Terminated;
-        _completeTime = currentTime;
+        CompleteTime = currentTime;
     }
 
-    public (TaskType, int, bool) Next()
+    /// <summary>
+    /// Bump to next task to simulate await on I/O task.
+    /// </summary>
+    /// <param name="task">the current I/O task for log.</param>
+    /// <returns>bool indicate if this process has completed.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public bool BumpToNext(out Task? task)
     {
-        // TODO: Need implementation
-        return (TaskType.CpuBounding, ArriveTime, false);
+        if (!_tasks.TryDequeue(out task))
+            throw new ArgumentException();
+        TimeHaveBurst += task.Duration;
+        return TimeHaveBurst >= BurstTime;
     }
 
-    public (TaskType?, int) Burst(int ticks)
+    /// <summary>
+    /// Burst 1 tick to simulate execution.
+    /// </summary>
+    /// <param name="clock"></param>
+    /// <returns></returns>
+    public Task? Burst(int clock)
     {
-        // TODO: Need implementation
-        TimeHaveBurst += ticks;
-        if (RemainingTime <= 0)
+        ++TimeHaveBurst;
+        
+        // process finished.
+        if (TimeHaveBurst >= BurstTime)
         {
-            return (null, 0);
+            SetComplete(clock);
+            return null;
         }
 
-        return (TaskType.CpuBounding, RemainingTime);
+        --_tasks.Peek().Duration;
+
+        return _tasks.Peek();
     }
 }
