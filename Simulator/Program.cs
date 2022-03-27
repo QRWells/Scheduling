09066f22ea36720b14f095d3e6090847173ba6f2
QRWells <mr.qrwang@gmail.com>
2022-03-28 05:57:32 +0900

    Remove unimplemented code in example.

--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -1,17 +1,12 @@
 ﻿using Simulator;
 using Simulator.Schedulers;
-using Terminal.Gui;
-
-Application.Run<App>();
-
-Console.WriteLine("Hello, World!");
 
 var os = new Os();
 var fifo = new FcfsScheduler();
 os.SetSchedule(fifo);
 
 var p1 = new Process(0);
-p1.AddTask(10,TaskType.CpuBounding);
+p1.AddTask(10);
 
 os.AddProcess(ref p1);
 
