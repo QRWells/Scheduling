654c5fd2065d01f9c48c61f21e82954b3defd2f4
QRWells <mr.qrwang@gmail.com>
2022-03-30 01:19:38 +0900

    Add Shortest Time-to-Completion First(STCF) scheduler.

---
+++ Simulator/Schedulers/StcfScheduler.cs
@@ -0,0 +1,54 @@
+﻿#region FileInfo
+
+// Copyright (c) 2022 Wang Qirui. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// 
+// This file is part of Project Simulator.
+// File Name   : StcfScheduler.cs
+// Author      : Qirui Wang
+// Created at  : 2022/03/25 6:27
+// Description :
+
+#endregion
+
+namespace Simulator.Schedulers;
+
+public class StcfScheduler : IScheduler
+{
+    private readonly PriorityQueue<int, int> _shortestJobFirstQueue = new();
+    private Os _os;
+
+    Os IScheduler.Os
+    {
+        get => _os;
+        set => _os = value;
+    }
+
+    public void OnProcessReady(int pid)
+    {
+        var process = _os.GetProcess(pid);
+        var burstTime = process.BurstTime;
+        _shortestJobFirstQueue.Enqueue(pid, burstTime);
+    }
+
+    public void SwitchProcess()
+    {
+        if (_shortestJobFirstQueue.TryDequeue(out var pid, out _))
+            _os.SwitchProcess(pid);
+        else
+            _os.Stop();
+    }
+
+    public void OnProcessBurst(int pid)
+    {
+        var remainingTime = _os.GetProcess(pid).RemainingTime;
+        
+        // Current process is shorter than the shortest job in the queue.
+        if (!_shortestJobFirstQueue.TryPeek(out var id, out var remain)
+            || remain >= remainingTime)
+            return;
+
+        SwitchProcess();
+        _shortestJobFirstQueue.Enqueue(pid, remainingTime);
+    }
+}
